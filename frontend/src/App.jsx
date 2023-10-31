f1c90882413320e12b8533108f00373feb4f0ce0
Leo <z5237478@ad.unsw.edu.au>
2023-11-01 02:06:45 +1100

    signup almost done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
+// import SignUp from './pages/Register';
+import SignUp from './pages/Signup';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from 'react-router-dom';
 
 function App () {
   return (
-    <>Let&apos;s go!</>
+    < BrowserRouter>
+      < Routes>
+        < Route path='/register' element={<SignUp/>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
