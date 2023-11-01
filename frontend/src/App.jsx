2d99894ba8c0bf95b213862d2b1c22ba78d14412
Eric Fu <fupengcheng231@gmail.com>
2023-11-01 20:59:29 +1100

    update context.js and login setter context

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,16 +7,32 @@ import {
 import Home from './pages/Home';
 import SignUp from './pages/Signup';
 import Login from './pages/Login';
+import { Context, initialValue } from './context';
 
 function App () {
+  const [email, setEmail] = React.useState(initialValue.email);
+  const [token, setToken] = React.useState(initialValue.token);
+  const [loggedIn, setLoggedIn] = React.useState(initialValue.loggedIn);
+  const getters = {
+    email,
+    token,
+    loggedIn,
+  }
+  const setters = {
+    setEmail,
+    setToken,
+    setLoggedIn,
+  }
   return (
-    <BrowserRouter>
-      <Routes>
-        < Route path='/' element={<Home/>} />
-        < Route path='/register' element={<SignUp/>} />
-        < Route path='/login' element={<Login/>} />
-      </Routes>
-    </BrowserRouter>
+    <Context.Provider value={{ getters, setters, }}>
+      <BrowserRouter>
+        <Routes>
+          < Route path='/' element={<Home/>} />
+          < Route path='/register' element={<SignUp/>} />
+          < Route path='/login' element={<Login/>} />
+        </Routes>
+      </BrowserRouter>
+    </Context.Provider>
   );
 }
 
