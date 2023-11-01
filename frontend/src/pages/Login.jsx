c82beb83d36ae47ed02733a16f6f1c64738fdcd2
Eric Fu <fupengcheng231@gmail.com>
2023-11-01 15:07:02 +1100

    update login

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -7,77 +7,76 @@ import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
+import CloseIcon from '@mui/icons-material/Close';
+import IconButton from '@mui/material/IconButton';
 
 export default function Login () {
   const navigate = useNavigate();
 
   const [openLogin, setOpenLogin] = React.useState(false);
 
-  // const returnHome = () => {
-  //   setOpenLogin(false);
-  //   navigate('/');
-  // }
+  // return home when login is closed
+  const handleCloseLogin = () => {
+    setOpenLogin(false);
+  };
 
   const handleClickOpenLogin = () => {
     setOpenLogin(true);
   };
 
-  const handleCloseLogin = () => {
-    setOpenLogin(false);
-  }
-
   const handleLoginForm = (e) => {
+    // TODO
     e.preventDefault();
-    const data = new FormData(e.target);
+    console.log(e);
+    const data = new FormData(e.currentTarget);
     console.log(data);
     setOpenLogin(false);
     navigate('/');
-  }
+  };
 
   return (
-    <React.Fragment>
-      <div>
-        <h1>Login</h1>
-        <Button onClick={() => {
-          navigate('/');
-        }}>
-          Home
+    <>
+      <React.Fragment>
+        <Button variant='outlined' onClick={handleClickOpenLogin}>
+          Login
         </Button>
-      </div>
-      <Button variant="outlined" onClick={handleClickOpenLogin}>
-        Open form dialog
-      </Button>
-      <Dialog open={openLogin} onClose={handleCloseLogin}>
-        <DialogTitle>Login</DialogTitle>
-        <DialogContent>
-          <DialogContentText>
-            To make booking, please enter your email address.
-          </DialogContentText>
-          <TextField
-            autoFocus
-            margin="dense"
-            id="name"
-            label="Email Address"
-            type="email"
-            fullWidth
-            variant="standard"
-          />
-          <TextField
-            autoFocus
-            margin="dense"
-            id="name"
-            label="Password"
-            type="password"
-            fullWidth
-            variant="standard"
-          />
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleCloseLogin}>Cancel</Button>
-          <Button onClick={handleLoginForm}>Login</Button>
-        </DialogActions>
-      </Dialog>
-
-    </React.Fragment>
+        <Dialog open={openLogin} onClose={handleCloseLogin}>
+          <IconButton
+            onClick={handleCloseLogin}
+            sx={{ position: 'absolute', right: 8, top: 8 }}
+            aria-label='close'>
+              <CloseIcon />
+          </IconButton>
+          <DialogTitle>Login</DialogTitle>
+          <DialogContent>
+            <DialogContentText>
+              To manage booking, please enter your email address and password.
+            </DialogContentText>
+            <TextField
+              autoFocus
+              margin='dense'
+              id='name'
+              label='Email Address'
+              type='email'
+              fullWidth
+              variant='standard'
+            />
+            <TextField
+              autoFocus
+              margin='dense'
+              id='name'
+              label='Password'
+              type='password'
+              fullWidth
+              variant='standard'
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleCloseLogin}>Cancel</Button>
+            <Button onClick={handleLoginForm}>Login</Button>
+          </DialogActions>
+        </Dialog>
+      </React.Fragment>
+    </>
   );
 }
