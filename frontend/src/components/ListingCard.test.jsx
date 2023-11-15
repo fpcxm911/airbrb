b5be68cc9249efa3f8d214d333a1fb6d86b6baad
Eric Fu <fupengcheng231@gmail.com>
2023-11-15 22:08:27 +1100

    finsih listingcard comp test

--- frontend/src/components/ListingCard.test.jsx
+++ frontend/src/components/ListingCard.test.jsx
@@ -56,7 +56,49 @@ it('rendering review ratings', () => {
   const result = render(
       <ListingCard listing = {listing} hotedPgae = {false}/>
   );
-  result.debug();
   expect(result.getByText(/2.5/i)).toBeInTheDocument();
   expect(result.getByText(/Number of reviews : 2/i)).toBeInTheDocument();
+});
+
+it('rendering different title', () => {
+  const listing = {
+    thumbnail: 'thumbnail.jpg',
+    title: 'different title',
+    metadata: {
+      propertyType: 'House',
+      numberOfBathrooms: 2,
+      bedrooms: [],
+    },
+    reviews: [
+      {
+        userEmail: 'cust@email.com',
+        bookingId: 799384397,
+        rating: 4,
+        comment: 'absolute boncus',
+        date: '2023-11-15T06:52:35.278Z'
+      },
+      {
+        userEmail: 'cust@email.com',
+        bookingId: 799384397,
+        rating: 3,
+        comment: 'insects bite me!!!',
+        date: '2023-11-15T06:53:05.811Z'
+      },
+      {
+        userEmail: 'cust@email.com',
+        bookingId: 799384397,
+        rating: 3,
+        comment: 'insects bite me!!!',
+        date: '2023-11-15T06:53:05.811Z'
+      }
+    ],
+  };
+
+  const result = render(
+      <ListingCard listing = {listing} hotedPgae = {false}/>
+  );
+  result.debug();
+  expect(result.getByText(/different title/i)).toBeInTheDocument();
+  expect(result.getByText(/Number of reviews : 3/i)).toBeInTheDocument();
+  expect(result.getByText(/3.3/i)).toBeInTheDocument();
 });
